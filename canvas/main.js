014dfcfedcd609b0fa8f6c8cb36719cf557f34dc
alisonys <452592978@qq.com>
2019-01-18 22:21:56 +0800

    upload finished canvas v0

--- canvas/main.js
+++ canvas/main.js
@@ -1,21 +1,12 @@
-var yyy = document.getElementById('xxx');
+var yyy = document.getElementById('myCanvas');
 var context = yyy.getContext('2d');
+var getWidth = 5;
+var getColor = 'black';
+var eraserEnabled = false;
 
 autoSetCanvasSize(yyy)
 
-listenToMouse(yyy)
-
-
-var eraserEnabled = false
-eraser.onclick = function() {
-  eraserEnabled =true
-  actions.className = 'actions x'
-
-}
-brush.onclick = function(){
-  eraserEnabled = false
-  actions.className = 'actions'
-}
+listenToUser(yyy)
 
 
 /******/
@@ -33,6 +24,7 @@ function autoSetCanvasSize(canvas) {
 
     canvas.width = pageWidth
     canvas.height = pageHeight
+    
   }
 }
 
@@ -45,17 +37,15 @@ function drawCircle(x, y, radius) {
 
 function drawLine(x1, y1, x2, y2) {
   context.beginPath();
-  context.strokeStyle = 'black'
+  context.strokeStyle = getColor
   context.moveTo(x1, y1) // 起点
-  context.lineWidth = 5
+  context.lineWidth = getWidth
   context.lineTo(x2, y2) // 终点
   context.stroke()
   context.closePath()
 }
 
-function listenToMouse(canvas) {
-
-
+function listenToUser(canvas) {
   var using = false
   var lastPoint = {
     x: undefined,
@@ -63,13 +53,14 @@ function listenToMouse(canvas) {
   }
 
   if(document.body.ontouchstart !== undefined) {
+    
     canvas.ontouchstart = function(aaa){
       var x = aaa.touches[0].clientX
       var y = aaa.touches[0].clientY
       console.log(x,y)
       using=true
       if (eraserEnabled) {
-        context.clearRect(x-5, y-5, 10, 10)
+        context.clearRect(x-15, y-15, 30, 30)
       } else {
         lastPoint = {
           "x": x,
@@ -84,7 +75,7 @@ function listenToMouse(canvas) {
       if (!using) {return}
 
       if (eraserEnabled) {
-        context.clearRect(x-5, y-5, 10, 10)
+        context.clearRect(x-15, y-15, 30, 30)
       } else {
         var newPoint = {
           "x": x,
@@ -103,7 +94,7 @@ function listenToMouse(canvas) {
       var y = aaa.clientY
       using = true
       if (eraserEnabled) {
-        context.clearRect(x-5, y-5, 10, 10)
+        context.clearRect(x-15, y-15, 30, 30)
       } else {
         lastPoint = {
           "x": x,
@@ -118,7 +109,7 @@ function listenToMouse(canvas) {
       if (!using) {return}
 
       if (eraserEnabled) {
-        context.clearRect(x-5, y-5, 10, 10)
+        context.clearRect(x-15, y-15, 30, 30)
       } else {
         var newPoint = {
           "x": x,
@@ -132,4 +123,78 @@ function listenToMouse(canvas) {
       using = false
     }
   }
+}
+
+/* Canvas Function List*/
+//Edit brush color
+black.onclick= function(){
+  getColor = 'black'
+  black.classList.add('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+  return getColor
+}
+red.onclick = function(){
+  getColor = 'red'
+  red.classList.add('active')
+  green.classList.remove('active')
+  blue.classList.remove('active')
+  black.classList.remove('active')
+  return getColor
+}
+green.onclick=function(){
+  getColor = 'green'
+  green.classList.add('active')
+  red.classList.remove('active')
+  blue.classList.remove('active')
+  black.classList.remove('active')
+  return getColor
+}
+blue.onclick=function(){
+  getColor = 'blue'
+  blue.classList.add('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+  black.classList.remove('active')
+  return getColor
+}
+
+//Edit brush width
+thin.onclick = function(){
+  getWidth = 5;
+  thin.classList.add('active')
+  bold.classList.remove('active')
+}
+bold.onclick = function(){
+  getWidth = 10
+  bold.classList.add('active')
+  thin.classList.remove('active')
+}
+
+//Switch between eraser and brush
+eraser.onclick = function() {
+  eraserEnabled =true
+  eraser.classList.add('active')
+  brush.classList.remove('active')
+}
+brush.onclick = function(){
+  eraserEnabled = false
+  brush.classList.add('active')
+  eraser.classList.remove('active')
+}
+
+//Clear all 
+clear.onclick=function(){
+  context.clearRect(0,0,yyy.width,yyy.height)
+}
+
+//Save your work
+save.onclick=function(){
+  var url = yyy.toDataURL('image/png')
+  var imgLink=document.createElement('a')
+  document.body.appendChild(imgLink)
+  imgLink.href=url
+  imgLink.download='canvas_'
+  imgLink.click()
 }
